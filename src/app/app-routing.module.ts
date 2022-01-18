b25dc3729f33791b04b1778fc1f593c411e3d612
Ben Luten <90648551+BenRegen@users.noreply.github.com>
2022-01-18 10:42:43 -0600

    Add AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthCompComponent } from './auth-comp/auth-comp.component';
 import { LogInComponent } from './auth-comp/log-in/log-in.component';
+import { AuthGuard } from './auth-guard/auth-guard.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:"", component:DashboardComponent},
-  {path:"login", component:AuthCompComponent}
+  {path:"login", component:AuthCompComponent, canActivate: [AuthGuard]},
+  {path:"login", component:LogInComponent, canActivate: [AuthGuard]},
 ];
 
-{
-  path: 'log-in',
-  component: LogInComponent,
-  canActivate: [AuthGuard],
-},
-}
+
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
--- src/app/auth-guard/auth-guard.component.ts
+++ src/app/auth-guard/auth-guard.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+//import { AuthService } from "./auth.service";
+import * as auth0 from "auth0-js";
+
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { map, take } from "rxjs/operators";
+
+@Injectable({ providedIn: "root" })
 
 @Component({
   selector: 'app-auth-guard',
@@ -12,4 +25,20 @@ export class AuthGuardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}
+
+export class AuthGuard implements CanActivate {
+  constructor(private authService: auth0, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.authService.currentUser.pipe(
+      take(1),
+      map((user) => {
+        const isAuth = !!user;
+
+        if (isAuth) return true;
+        if (!isAuth) return this.router.createUrlTree(["auth"]);
+      })
+    );
+  }
 }
